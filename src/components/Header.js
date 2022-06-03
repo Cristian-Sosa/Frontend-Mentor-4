26990c667acaa4d02202062d1a78e05486ad8d67
Cristian-Sosa <64815792+Cristian-Sosa@users.noreply.github.com>
2022-06-03 20:54:38 -0300

    feat: create header and styles

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header({ title, description, imageUrl }) {
+  return (
+    <header className="header">
+      <h1 className="title">Reliable efficient delivery</h1>
+      <h2 className="subtitle">Powered by Technology</h2>
+
+      <span className='description'>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</span>
+    </header>
+  );
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/dist/index.css';
 import Main from './components/Main';
+import Header from './components/Header'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode>    
+    <Header />
     <Main />
   </React.StrictMode>
 );
